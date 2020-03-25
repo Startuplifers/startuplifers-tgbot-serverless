c8b887643cc24be288623b6ac21d004f11c27ebd
Juho Toivonen <juho.toivonen@aalto.fi>
2020-03-25 15:16:18 +0200

    Added support for multiple channels and updated package-lock

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ dotenv.config();
 const apiUrl = "https://api.lever.co/v0/postings/startuplifers?mode=json";
 const fileName = "jobs.json";
 const bucketName = process.env.BUCKET_NAME;
-const channelName = "@testikanava123";
+const channels = Object.freeze({
+  Business:      "@startuplifersbusiness",
+  Design:        "@startuplifersdesign",
+  Engineering:   "@startuplifersengineering",
+  Other:         null
+});
 
 export async function main() {
   let storedJobs;
@@ -21,9 +26,12 @@ export async function main() {
   );
 
   if (newJobs) {
-    const promises = newJobs.map(job =>
-      bot.sendMessage(channelName, `${job.hostedUrl}\n\n${job.text}`)
-    );
+    const promises = newJobs.map(job => {
+      const channelName = channels[job.categories.department]
+      if (channelName) {
+        return bot.sendMessage(channelName, `${job.hostedUrl}\n\nPosition: ${job.text}\nLocation: ${job.categories.location}\nCommitment: ${job.categories.commitment}`)
+      }
+    });
     await Promise.all(promises);
     try {
       await storeJobs(bucketName, fileName, apiJobs);
